a719652c69161b9c63bdbedcf6db7ecb2316fa5e
Primordial <64266457+ConceptualGhost@users.noreply.github.com>
2024-07-06 17:19:07 +0200

    Functional Hide
    Hide from Duck

--- Scripts/PlayerDetectionArea.gd
+++ Scripts/PlayerDetectionArea.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
 @export var duck_stats : CharacterBody2D
-
+@onready var player_stats = get_node("/root/PlayerStats")
 
 func _body_entered(body):
-	if "Player" in body.get_groups():
+	if "Player" in body.get_groups() and player_stats.Hidden == false:
 		duck_stats.is_agro = true
 	pass 
