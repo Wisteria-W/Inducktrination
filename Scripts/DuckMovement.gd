d4f9bcd7602fa46934e330534addba0eb6db5cc7
Primordial <64266457+ConceptualGhost@users.noreply.github.com>
2024-07-06 19:07:34 +0200

    Box Duckterritory
    Box Duck

--- Scripts/DuckMovement.gd
+++ Scripts/DuckMovement.gd
@@ -4,12 +4,25 @@ extends CharacterBody2D
 @onready var player_stats = get_node("/root/PlayerStats")
 
 @export var movement_speed : float
+var x_min
+var x_max
+var y_min
+var y_max
+var x_center
+var y_center
 
 var is_agro : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	nav_agent.target_position = Vector2(position.x + randf_range(-30, 30), position.y + randf_range(-30, 30))
-	pass # Replace with function body.
+	x_min = position.x - 100
+	x_max = position.x + 100
+	y_min = position.y - 100
+	y_max = position.y + 100
+	x_center = position.x 
+	y_center = position.y
+
+pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,6 +41,15 @@ func _physics_process(delta):
 func random_movement():
 	if position.distance_to(nav_agent.target_position) < 5:
 		var movement_vector = Vector2(position.x + randf_range(-300, 300), position.y + randf_range(-30, 30))
+		if x_max < movement_vector.x : 
+			movement_vector.x = x_center
+		if x_min > movement_vector.x : 
+			movement_vector.x = x_center
+		if y_max < movement_vector.y : 
+			movement_vector.y = y_center
+		if y_min > movement_vector.y : 
+			movement_vector.y = y_center
+
 		nav_agent.target_position = movement_vector
 	pass
 func agro_player():
