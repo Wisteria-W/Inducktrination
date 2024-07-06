727ae1129d00e3f76056e8df53b8b9af2ae76df3
Stas Vysotska <anastasiia.vysotska@ue-germany.de>
2024-07-06 16:47:55 +0200

    [ADD] Dialogue when too close to Duck, dlg_test dialogue

--- Scripts/PlayerConversationArea.gd
+++ Scripts/PlayerConversationArea.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+@export var small_talks : Array[DialogueResource]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +9,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func body_entered(body):
+	if "Player" in body.get_groups():
+		DialogueManager.show_dialogue_balloon(small_talks.pick_random())
+	pass # Replace with function body.
